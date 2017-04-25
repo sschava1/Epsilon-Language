701678bf3a2ad8148c04109a6f051cbc11054d13
Chiraag Subramanian <chiraag.subramanian@asu.edu>
2017-04-24 18:12:49 -0700

    Epsilon project structure.

---
+++ Epsilon/src/com/epsilon/EpsilonRunner.java
@@ -0,0 +1,20 @@
+package com.epsilon;
+/**
+ * @purpose
+ * 		The Epsilon Runner class is responsible for reading the source program
+ * 		and invoking the Epsilon parser to parse the source program.
+ *      Writes the generated intermediate code to a file.
+ * 
+ * @version  %I%, %G% 
+ * 			
+ * @author 
+ *
+ */
+public class EpsilonRunner {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
