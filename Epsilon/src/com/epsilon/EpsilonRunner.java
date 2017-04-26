2dd2ac24c6a9a6776f8fa9ff3bb30b9f24c2ce42
Pranjal Karankar <pranjalsk@users.noreply.github.com>
2017-04-25 23:18:57 -0700

    Updating EpsilonRunner

--- Epsilon/src/com/epsilon/EpsilonRunner.java
+++ Epsilon/src/com/epsilon/EpsilonRunner.java
@@ -1,4 +1,11 @@
 package com.epsilon;
+import java.io.FileInputStream;
+import java.io.PrintWriter;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 /**
  * @purpose
  * 		The Epsilon Runner class is responsible for reading the source program
@@ -11,10 +18,29 @@ package com.epsilon;
  *
  */
 public class EpsilonRunner {
+	public static void main(String[] args) throws Exception {	
+	//to take  file as input
+		FileInputStream fileInputStream = new FileInputStream(args[0]);		
+		ANTLRInputStream input = new ANTLRInputStream(fileInputStream);
 
-	public static void main(String[] args) {
+		HelloLexer lexer = new HelloLexer(input);
+
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+		HelloParser parser = new HelloParser(tokens);		
+		ParserRuleContext tree = parser.start();
+		ParseTreeWalker walker = new ParseTreeWalker(); // parse tree walker object
+		NewListener listener = new NewListener();
 		
-
+		try {
+			PrintWriter writer = new PrintWriter("parseTree.pt", "UTF-8");
+			writer.println(tree);	
+			writer.println(tree.toStringTree(parser));			
+			writer.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("\n Exception caught"+ e.toString());
+		}			
+		
 	}
-
 }
