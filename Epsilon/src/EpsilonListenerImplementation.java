3c803fc9bd57659e941d6d01a69cacc92433b6d5
pranjalsk <pkaranka@asu.edu>
2017-04-29 14:29:55 -0700

    updating listener

--- Epsilon/src/EpsilonListenerImplementation.java
+++ Epsilon/src/EpsilonListenerImplementation.java
@@ -3,267 +3,326 @@ import java.util.*;
 
 import org.antlr.v4.runtime.misc.NotNull;
 
-public class EpsilonListenerImplementation extends EpsilonBaseListener{
+public class EpsilonListenerImplementation extends EpsilonBaseListener {
 
+	
+	List<String> ind = new ArrayList<String>();	
+	Stack<Integer> loopStart = new Stack<Integer>();
+	Stack<Integer> loopCondition = new Stack<Integer>();
+	Stack<Integer> ifElseCount = new Stack<Integer>();
+	Stack<Integer> ifElseCondition = new Stack<Integer>();
+	Stack<Integer> ifElseEnd = new Stack<Integer>();
 	int mainCnt = 0;
 	int lineCount = 1;
-	
-	@Override 
+
+	@Override
 	public void enterStart(@NotNull EpsilonParser.StartContext ctx) {
-		if(mainCnt<1){
+		if (mainCnt < 1) {
 			ind.add("");
 			mainCnt++;
 		}
 	}
-	
+
 	@Override
 	public void exitStart(@NotNull EpsilonParser.StartContext ctx) {
 		ind.add("EXIT ");
 	}
-	
-	@Override 
-	public void exitPrint(@NotNull EpsilonParser.PrintContext ctx) { 
+
+	@Override
+	public void exitPrint(@NotNull EpsilonParser.PrintContext ctx) {
 		lineCount++;
 		ind.add("PRINT ");
 	}
-	
-	@Override 
-	public void enterDefinitionDeclaration(@NotNull EpsilonParser.DefinitionDeclarationContext ctx) { 
+
+	@Override
+	public void enterDefinitionDeclaration(@NotNull EpsilonParser.DefinitionDeclarationContext ctx) {
 		lineCount++;
 		ind.add("DEFN " + ctx.IDENTIFIER());
-		if(ctx.definitionParameters().identifierDeclaration() != null){
-			for(EpsilonParser.IdentifierDeclarationContext obj : ctx.definitionParameters().identifierDeclaration()){
+		if (ctx.definitionParameters().identifierDeclaration() != null) {
+			for (EpsilonParser.IdentifierDeclarationContext obj : ctx.definitionParameters().identifierDeclaration()) {
 				System.out.println(obj.IDENTIFIER());
 				lineCount++;
 				ind.add("SAVE " + obj.IDENTIFIER());
 			}
-		}		
+		}
 	}
-	
-	@Override 
+
+	@Override
 	public void exitDefinitionDeclaration(@NotNull EpsilonParser.DefinitionDeclarationContext ctx) {
 		lineCount++;
 		ind.add("EXITDEFN ");
 	}
-	
-	@Override 
+
+	@Override
 	public void exitDefinitionInvocation(@NotNull EpsilonParser.DefinitionInvocationContext ctx) {
 		lineCount++;
 		System.out.println(ctx.IDENTIFIER());
 		ind.add("INVOKE " + ctx.IDENTIFIER());
 	}
-	
-	@Override 
+
+	@Override
 	public void enterDataType(@NotNull EpsilonParser.DataTypeContext ctx) {
 		lineCount++;
-		if(ctx.NUMERIC() != null){
+		if (ctx.NUMERIC() != null) {
 			ind.add("PUSH " + ctx.NUMERIC());
-		}
-		else if (ctx.BOOL()!=null) {
+		} else if (ctx.BOOL() != null) {
 			ind.add("PUSH " + ctx.BOOL());
-		}
-		else{
+		} else {
 			ind.add("PUSH " + ctx.IDENTIFIER());
 		}
 	}
-	
-	@Override 
+
+	@Override
 	public void exitDefinitionReturn(@NotNull EpsilonParser.DefinitionReturnContext ctx) {
 		lineCount++;
-		if(ctx.IDENTIFIER()!=null){
+		if (ctx.IDENTIFIER() != null) {
 			ind.add("RETURN " + ctx.IDENTIFIER());
-		}
-		else if (ctx.NUMERIC()!=null) {
+		} else if (ctx.NUMERIC() != null) {
 			ind.add("RETURN " + ctx.NUMERIC());
-		}
-		else if (ctx.BOOL() != null) {
+		} else if (ctx.BOOL() != null) {
 			ind.add("RETURN " + ctx.BOOL());
-		}
-		else
-		{
+		} else {
 			ind.add("SAVE temp");
 			lineCount++;
 			ind.add("RETURN temp");
 		}
 	}
-	
-	@Override 
-	public void enterMainIfStatement(@NotNull EpsilonParser.MainIfStatementContext ctx) { }
+
+	@Override
+	public void enterMainIfStatement(@NotNull EpsilonParser.MainIfStatementContext ctx) {
+	}
+
 	/**
 	 * {@inheritDoc}
 	 *
-	 * <p>The default implementation does nothing.</p>
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
 	 */
 	@Override
-	public void exitMainIfStatement(@NotNull EpsilonParser.MainIfStatementContext ctx) { }
+	public void exitMainIfStatement(@NotNull EpsilonParser.MainIfStatementContext ctx) {
+	}
+
 	/**
 	 * {@inheritDoc}
 	 *
-	 * <p>The default implementation does nothing.</p>
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
 	 */
-	
-	@Override 
-	public void enterIfelseStatement(@NotNull EpsilonParser.IfelseStatementContext ctx) { }
+
+	@Override
+	public void enterIfelseStatement(@NotNull EpsilonParser.IfelseStatementContext ctx) {
+	}
+
 	/**
 	 * {@inheritDoc}
 	 *
-	 * <p>The default implementation does nothing.</p>
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
 	 */
-	@Override 
-	public void exitIfelseStatement(@NotNull EpsilonParser.IfelseStatementContext ctx) { }
+	@Override
+	public void exitIfelseStatement(@NotNull EpsilonParser.IfelseStatementContext ctx) {
+	}
+
 	/**
 	 * {@inheritDoc}
 	 *
-	 * <p>The default implementation does nothing.</p>
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
 	 */
-	@Override 
-	public void enterPrefElseIf(@NotNull EpsilonParser.PrefElseIfContext ctx) { }
+	@Override
+	public void enterPrefElseIf(@NotNull EpsilonParser.PrefElseIfContext ctx) {
+	}
+
 	/**
 	 * {@inheritDoc}
 	 *
-	 * <p>The default implementation does nothing.</p>
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
 	 */
-	@Override 
-	public void exitPrefElseIf(@NotNull EpsilonParser.PrefElseIfContext ctx) { }
+	@Override
+	public void exitPrefElseIf(@NotNull EpsilonParser.PrefElseIfContext ctx) {
+	}
+
 	/**
 	 * {@inheritDoc}
 	 *
-	 * <p>The default implementation does nothing.</p>
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
 	 */
-	@Override 
-	public void enterIfStatement(@NotNull EpsilonParser.IfStatementContext ctx) { }
+	@Override
+	public void enterIfStatement(@NotNull EpsilonParser.IfStatementContext ctx) {
+	}
+
 	/**
 	 * {@inheritDoc}
 	 *
-	 * <p>The default implementation does nothing.</p>
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
 	 */
-	@Override 
-	public void exitIfStatement(@NotNull EpsilonParser.IfStatementContext ctx) { }
+	@Override
+	public void exitIfStatement(@NotNull EpsilonParser.IfStatementContext ctx) {
+	}
+
 	/**
 	 * {@inheritDoc}
 	 *
-	 * <p>The default implementation does nothing.</p>
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
 	 */
-	@Override 
-	public void enterElseIfStatement(@NotNull EpsilonParser.ElseIfStatementContext ctx) { }
+	@Override
+	public void enterElseIfStatement(@NotNull EpsilonParser.ElseIfStatementContext ctx) {
+	}
+
 	/**
 	 * {@inheritDoc}
 	 *
-	 * <p>The default implementation does nothing.</p>
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
 	 */
-	@Override 
-	public void exitElseIfStatement(@NotNull EpsilonParser.ElseIfStatementContext ctx) { }
+	@Override
+	public void exitElseIfStatement(@NotNull EpsilonParser.ElseIfStatementContext ctx) {
+	}
 	/*
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	
-	@Override 
-	public void enterPrefIf(@NotNull EpsilonParser.PrefIfContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override 
-	public void exitPrefIf(@NotNull EpsilonParser.PrefIfContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override 
-	public void enterElseStatement(@NotNull EpsilonParser.ElseStatementContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override 
-	public void exitElseStatement(@NotNull EpsilonParser.ElseStatementContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
 
-	@Override 
-	public void enterMainElseStatement(@NotNull EpsilonParser.MainElseStatementContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override 
-	public void exitMainElseStatement(@NotNull EpsilonParser.MainElseStatementContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	
-	@Override 
-	public void enterIdentifierDeclarationAssignment(@NotNull EpsilonParser.IdentifierDeclarationAssignmentContext ctx) {
-		
+	@Override
+	public void enterPrefIf(@NotNull EpsilonParser.PrefIfContext ctx) {
 	}
 
-	@Override 
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
+	 */
+	@Override
+	public void exitPrefIf(@NotNull EpsilonParser.PrefIfContext ctx) {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
+	 */
+	@Override
+	public void enterElseStatement(@NotNull EpsilonParser.ElseStatementContext ctx) {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
+	 */
+	@Override
+	public void exitElseStatement(@NotNull EpsilonParser.ElseStatementContext ctx) {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
+	 */
+
+	@Override
+	public void enterMainElseStatement(@NotNull EpsilonParser.MainElseStatementContext ctx) {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
+	 */
+	@Override
+	public void exitMainElseStatement(@NotNull EpsilonParser.MainElseStatementContext ctx) {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>
+	 * The default implementation does nothing.
+	 * </p>
+	 */
+
+	@Override
+	public void enterIdentifierDeclarationAssignment(
+			@NotNull EpsilonParser.IdentifierDeclarationAssignmentContext ctx) {
+
+	}
+
+	@Override
 	public void exitIdentifierDeclarationAssignment(@NotNull EpsilonParser.IdentifierDeclarationAssignmentContext ctx) {
 		lineCount++;
 		ind.add("SAVE " + ctx.IDENTIFIER());
 	}
-	@Override 
-	public void enterIdentifierAssignment(@NotNull EpsilonParser.IdentifierAssignmentContext ctx) { 
-		
+
+	@Override
+	public void enterIdentifierAssignment(@NotNull EpsilonParser.IdentifierAssignmentContext ctx) {
+
 	}
-	@Override 
-	public void exitIdentifierAssignment(@NotNull EpsilonParser.IdentifierAssignmentContext ctx) { 
+
+	@Override
+	public void exitIdentifierAssignment(@NotNull EpsilonParser.IdentifierAssignmentContext ctx) {
 		lineCount++;
 		ind.add("SAVE " + ctx.IDENTIFIER());
 	}
 
-	@Override public void enterExpression(@NotNull EpsilonParser.ExpressionContext ctx) { 
-		
-	
+	@Override
+	public void enterExpression(@NotNull EpsilonParser.ExpressionContext ctx) {
+
 	}
 
-	@Override public void exitExpression(@NotNull EpsilonParser.ExpressionContext ctx) { 
-		
+	@Override
+	public void exitExpression(@NotNull EpsilonParser.ExpressionContext ctx) {
+
 		lineCount++;
-		if(ctx.ADD()!=null){
+		if (ctx.ADD() != null) {
 			ind.add("ADD");
-		}
-		else if(ctx.SUB() != null){
+		} else if (ctx.SUB() != null) {
 			ind.add("SUB");
-		}
-		else if(ctx.MUL() != null){
+		} else if (ctx.MUL() != null) {
 			ind.add("MUL");
-		}
-		else if(ctx.DIV()!= null){
+		} else if (ctx.DIV() != null) {
 			ind.add("DIV");
-		}
-		else if(ctx.POWER() != null){
+		} else if (ctx.POWER() != null) {
 			ind.add("POW");
 		}
-				
+
 	}
-	
-	@Override 
+
+	@Override
 	public void enterBoolExpression(@NotNull EpsilonParser.BoolExpressionContext ctx) {
-		
+
 		lineCount++;
-		if(ctx.LOGICAND() != null){
+		if (ctx.LOGICAND() != null) {
 			ind.add("AND");
-		}
-		else if(ctx.LOGICNOT() != null){
+		} else if (ctx.LOGICNOT() != null) {
 			ind.add("NOT");
-		}
-		else if(ctx.LOGICOR()!=null){
+		} else if (ctx.LOGICOR() != null) {
 			ind.add("OR");
-		}
-		else if(ctx.COMPARATORS()!=null){
-			switch(ctx.COMPARATORS().toString()){
+		} else if (ctx.COMPARATORS() != null) {
+			switch (ctx.COMPARATORS().toString()) {
 			case ">":
 				ind.add("GREATER");
 				break;
@@ -278,25 +337,14 @@ public class EpsilonListenerImplementation extends EpsilonBaseListener{
 				break;
 			case "==":
 				ind.add("EQUALS");
-				break;	
-}
+				break;
+			}
 		}
 	}
 
-	@Override 
-	public void exitBoolExpression(@NotNull EpsilonParser.BoolExpressionContext ctx) { 
-		
+	@Override
+	public void exitBoolExpression(@NotNull EpsilonParser.BoolExpressionContext ctx) {
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
